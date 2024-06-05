30cb4caa5d2d8b3b7b56c07c8780bb50f8dfe52a
vkahara <valtterikahara@gmail.com>
2024-06-05 11:11:16 +0300

    integrated mongodb to graphql server using mongoose

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+//mongodb uri from dotenv
+const MONGODB_URI = process.env.MONGODB_URI;
+
+//set strickquery to false, remember to change later for prod
+mongoose.set("strictQuery", false);
+mongoose.connect(MONGODB_URI);
+
+const db = mongoose.connection;
+//handle db connection error
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to database");
+});
+
+const server = new ApolloServer({ typeDefs, resolvers, contex: { db } });
 
 server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+  console.log(`🙏🙏🙏 Server ready at ${url}`);
 });
--- resolvers.js
+++ resolvers.js
@@ -1,37 +1,30 @@
 const { v4: uuidv4 } = require("uuid");
-
-let users = [
-  //testestsets create db later
-  {
-    id: uuidv4(),
-    username: "valtteri",
-    password: "labra123",
-    squatMax: 195,
-    benchMax: 102.5,
-    deadliftMax: 205
-  }
-];
+const User = require("./models/Users");
 
 const resolvers = {
   Query: {
-    user: (parent, args, context, info) => {
-      return users.find((user) => user.username === args.username);
+    //get one user
+    user: async (parent, args, context, info) => {
+      return await User.findOne({ username: args.username });
     },
-    users: () => users
+    //get all users
+    users: async () => {
+      return await User.find();
+    }
   },
 
   Mutation: {
-    createUser: (parent, args, context, info) => {
-      const newUser = {
+    //create new user
+    createUser: async (parent, args, context, info) => {
+      const newUser = new User({
         id: uuidv4(),
         username: args.username,
         password: args.password,
         squatMax: args.squatMax,
         benchMax: args.benchMax,
         deadliftMax: args.deadliftMax
-      };
-      users.push(newUser);
-      return newUser;
+      });
+      return await newUser.save();
     }
   }
 };
