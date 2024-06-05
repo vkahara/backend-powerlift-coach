a47a697163e529972d2bb2348366f60c72a5f1e6
vkahara <valtterikahara@gmail.com>
2024-06-05 14:28:18 +0300

    new branch, weeks.js editing

--- utils/weeks.js
+++ utils/weeks.js
@@ -1,3 +1,5 @@
+const { squatMax, benchMax, deadliftMax } = maxLifts;
+
 const weeks = {
     week1: {
         day1: {
@@ -6,7 +8,7 @@ const weeks = {
                 sets: 4,
                 reps: 7,
                 intensity: 0.67,
-                load: null,
+                load: Math.round(squatMax * 0.67),
             },
             exercise2: {
                 name: "Paused Bench",
@@ -16,11 +18,14 @@ const weeks = {
                 load: null,
             },
             exercise3: {
-                name: "Overload Press",
+                name: "Overhead Press",
                 sets: 3,
                 reps: 8,
                 intensity: null,
                 load: null,
+            },
+            exercise4: {
+                name: "Bent Over Row"
             }
         },
     },
