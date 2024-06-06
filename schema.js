c22c05070a8e269054dae5fd521711cc784abc6d
vkahara <valtterikahara@gmail.com>
2024-06-06 09:39:38 +0300

    schema queries

--- schema.js
+++ schema.js
@@ -4,6 +4,9 @@ const typeDefs = gql`
   type Query {
     user(username: String!): User
     users: [User]
+    exercise(username: String!, week: Int!, day: Int!, exercise: Int!): Exercise)
+    day(username: String!, week: Int!, day: Int!): Day
+    week(username: String!, week: Int!): Week
   }
   type Mutation {
     createUser(
